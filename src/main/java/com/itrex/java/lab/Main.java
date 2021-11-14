b12ab46def3bdc7112996eb0f2fed7fc8980ba2e
Illia Yaromkin <dom1nik@mail.ru>
2021-11-14 17:05:46 +0300

    "complete homework"

--- src/main/java/com/itrex/java/lab/Main.java
+++ src/main/java/com/itrex/java/lab/Main.java
@@ -1,11 +1,7 @@
 package com.itrex.java.lab;
 
-import com.itrex.java.lab.exeption.RepositoryException;
-import com.itrex.java.lab.exeption.ServiceException;
-
 public class Main {
 
-    public static void main(String[] args) throws ServiceException, RepositoryException {
-
+    public static void main(String[] args) {
     }
 }
